27ca61f20469fe19ad6919da93205ab46149d687
KaanGokdogan <kaan.gkdoan93@gmail.com>
2024-11-28 17:52:12 +0300

    Use of led and button same time

---
+++ stm32f4xx_drivers/Src/002Led_Toggle_with_Button.c
@@ -0,0 +1,56 @@
+/*
+ * 002Led_Toggle_with_Button.c
+ *
+ *  Created on: Nov 28, 2024
+ *      Author: MONSTER
+ */
+
+#include "stm32f407xx.h"
+
+#define HIGH 1
+#define BTN_PRESSED HIGH
+
+void delay(void)
+{
+	for(uint32_t i = 0; i < 50000000; i++);
+}
+
+int main()
+{
+
+	GPIO_Handle_t gpioLed,gpioButton;
+
+	// This is led configuration
+	gpioLed.pGPIOx = GPIOD;
+	gpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
+	gpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	gpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	gpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP; // OD olursa led yanmıyor ama Pull up yaparsan OD deyken çok az bir voltaj geçtiği için led az yanıyor.
+	gpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_PeriClockControl(GPIOD, ENABLE);
+	GPIO_Init(&gpioLed);
+
+	// This is button configuration
+	gpioLed.pGPIOx = GPIOA;
+	gpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_0;
+	gpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+	gpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	gpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_PeriClockControl(GPIOA, ENABLE);
+	GPIO_Init(&gpioButton);
+
+
+
+	for(;;)
+	{
+		if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0) == BTN_PRESSED)
+		{
+			delay();
+			GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
+		}
+
+	}
+	return 0;
+}
