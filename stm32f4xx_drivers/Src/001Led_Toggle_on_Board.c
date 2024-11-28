2fad6ee753341ed5089e93b48f2d3f5caaaf3e1f
KaanGokdogan <kaan.gkdoan93@gmail.com>
2024-11-28 17:43:55 +0300

    Toogle led button application

---
+++ stm32f4xx_drivers/Src/001Led_Toggle_on_Board.c
@@ -0,0 +1,37 @@
+/*
+ * 001Led_Toggle_on_Board.c
+ *
+ *  Created on: Nov 28, 2024
+ *      Author: MONSTER
+ */
+
+#include "stm32f407xx.h"
+
+void delay(void)
+{
+	for(uint32_t i = 0; i < 50000000; i++);
+}
+
+int main()
+{
+
+	GPIO_Handle_t gpioLed;
+
+	gpioLed.pGPIOx = GPIOD;
+	gpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
+	gpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	gpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	gpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP; // OD olursa led yanmıyor ama Pull up yaparsan OD deyken çok az bir voltaj geçtiği için led az yanıyor.
+	gpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_PeriClockControl(GPIOD, ENABLE);
+	GPIO_Init(&gpioLed);
+
+
+	for(;;)
+	{
+		GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
+		delay();
+	}
+	return 0;
+}
