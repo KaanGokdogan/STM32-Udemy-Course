9d59480f4cf0b1834c0e0e730d1e9d4665cd6367
KaanGokdogan <kaan.gkdoan93@gmail.com>
2024-12-10 19:14:53 +0300

    SPI send data function

--- stm32f4xx_drivers/drivers/Src/stm32f407xx_driver_spi_driver.c
+++ stm32f4xx_drivers/drivers/Src/stm32f407xx_driver_spi_driver.c
@@ -6,3 +6,284 @@
  */
 
 #include "stm32f407xx_driver_spi_driver.h"
+
+
+
+uint8_t SPI_GetFlagStatus(SPI_RegDef_t *pSPIx, uint32_t FlagName)
+{
+	if(pSPIx->SR & FlagName)
+	{
+		return FLAG_SET;
+	}
+
+	return FLAG_RESET;
+}
+/*
+ * Peripheral Clock setup
+ */
+/************************************************************************
+ * @fn				- SPI_PeriClockControl
+ *
+ * @brief			-
+ *
+ * @param[in]		-
+ * @param[in]		-
+ * @param[in]		-
+ *
+ * @return			- none
+ *
+ * @Note			- none
+ ************************************************************************/
+void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi)
+{
+	if(EnorDi == ENABLE)
+	{
+		if(pSPIx == SPI1)
+		{
+			SPI1_PCLK_EN();
+		}
+		else if(pSPIx == SPI2)
+		{
+			SPI2_PCLK_EN();
+		}
+		else if(pSPIx == SPI3)
+		{
+			SPI3_PCLK_EN();
+		}
+		else if(pSPIx == SPI4)
+		{
+			SPI4_PCLK_EN();
+		}
+	}
+	else
+	{
+		if(pSPIx == SPI1)
+		{
+			SPI1_PCLK_DI();
+		}
+		else if(pSPIx == SPI2)
+		{
+			SPI2_PCLK_DI();
+		}
+		else if(pSPIx == SPI3)
+		{
+			SPI3_PCLK_DI();
+		}
+		else if(pSPIx == SPI4)
+		{
+			SPI4_PCLK_DI();
+		}
+	}
+}
+/*
+ * Init and De-init
+ */
+/************************************************************************
+ * @fn				- SPI_Init
+ *
+ * @brief			-
+ *
+ * @param[in]		-
+ * @param[in]		-
+ * @param[in]		-
+ *
+ * @return			- none
+ *
+ * @Note			- none
+ ************************************************************************/
+void SPI_Init(SPI_Handle_t *pSPIHandle)
+{
+	// first lets configure the SPI_CR1 register
+	uint32_t tempReg = 0;
+
+	// 1. Configure the device mode
+	tempReg |= pSPIHandle->SPI_Config.SPI_DeviceMode << SPI_CR1_MSTR;
+
+	// 2. Configure the bus config
+	if(pSPIHandle->SPI_Config.SPI_BusConfig == SPI_BUS_CONFIG_FD)
+	{
+		// bidi mode should be cleared
+		tempReg &= ~(1 << SPI_CR1_BIDIMODE);
+	}
+	else if(pSPIHandle->SPI_Config.SPI_BusConfig == SPI_BUS_CONFIG_HD)
+	{
+		// bidi mode should be set
+		tempReg |= (1 << SPI_CR1_BIDIMODE);
+	}
+	else if(pSPIHandle->SPI_Config.SPI_BusConfig == SPI_BUS_CONFIG_SIMPLEX_RXONLY)
+	{
+		// bidi mode should be cleared
+		tempReg &= ~(1 << SPI_CR1_BIDIMODE);
+
+		// bidi mode should be set
+		tempReg |= (1 << SPI_CR1_RXONLY);
+	}
+
+	// 3. Configure the DFF
+	tempReg |= pSPIHandle->SPI_Config.SPI_DFF << SPI_CR1_DFF;
+	// 4 Configure the CPHA
+	tempReg |= pSPIHandle->SPI_Config.SPI_CPHA << SPI_CR1_CPHA;
+	// 5. Configure the CPOL
+	tempReg |= pSPIHandle->SPI_Config.SPI_CPOL << SPI_CR1_CPOL;
+	// 6. Configure the SPI serial clock speed (baud rate)
+	tempReg |= pSPIHandle->SPI_Config.SPI_SclkSpeed << SPI_CR1_BR;
+
+	pSPIHandle->pSPIx->CR1 = tempReg;
+
+
+
+
+}
+
+/************************************************************************
+ * @fn				- SPI_DeInit
+ *
+ * @brief			-
+ *
+ * @param[in]		-
+ * @param[in]		-
+ * @param[in]		-
+ *
+ * @return			- none
+ *
+ * @Note			- none
+ ************************************************************************/
+void SPI_DeInit(SPI_RegDef_t *pSPIx)
+{
+	if(pSPIx == SPI1)
+	{
+		SPI1_REG_RESET();
+	}
+	else if(pSPIx == SPI2)
+	{
+		SPI2_REG_RESET();
+	}
+	else if(pSPIx == SPI3)
+	{
+		SPI3_REG_RESET();
+	}
+	else if(pSPIx == SPI4)
+	{
+		SPI4_REG_RESET();
+	}
+}
+
+/*
+ * Data Send and Receive
+ */
+/************************************************************************
+ * @fn				- SPI_SendData
+ *
+ * @brief			-
+ *
+ * @param[in]		-
+ * @param[in]		-
+ * @param[in]		-
+ *
+ * @return			- none
+ *
+ * @Note			- This is blocking call
+ ************************************************************************/
+void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer,uint32_t Length)
+{
+	while(Length > 0)
+	{
+		// 1. Wait until TXE is set
+		// potential to block permanently, POLLING
+		while(SPI_GetFlagStatus(pSPIx, SPI_TXE_FLAG) == FLAG_RESET);
+
+		// 2. check the DFF bit in CR1
+		if(pSPIx->CR1 & (1 << SPI_CR1_DFF))
+		{
+			// 16 bit DFF
+			// 1. load the data in to the DR
+			pSPIx->DR = *( (uint16_t*)pTxBuffer);
+			Length--;
+			Length--;
+			(uint16_t*)pTxBuffer++;
+		}
+		else
+		{
+			// 8 bit DFF
+			pSPIx->DR = *pTxBuffer;
+			Length--;
+			pTxBuffer++;
+		}
+	}
+}
+/************************************************************************
+ * @fn				- SPI_ReceiveData
+ *
+ * @brief			-
+ *
+ * @param[in]		-
+ * @param[in]		-
+ * @param[in]		-
+ *
+ * @return			- none
+ *
+ * @Note			- none
+ ************************************************************************/
+void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer,uint32_t Length)
+{
+
+}
+
+
+/*
+ * IRQ Configuration and ISR handling
+ */
+/************************************************************************
+ * @fn				- SPI_ReceiveData
+ *
+ * @brief			-
+ *
+ * @param[in]		-
+ * @param[in]		-
+ * @param[in]		-
+ *
+ * @return			- none
+ *
+ * @Note			- none
+ ************************************************************************/
+void SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi)
+{
+
+}
+/************************************************************************
+ * @fn				- SPI_ReceiveData
+ *
+ * @brief			-
+ *
+ * @param[in]		-
+ * @param[in]		-
+ * @param[in]		-
+ *
+ * @return			- none
+ *
+ * @Note			- none
+ ************************************************************************/
+void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority)
+{
+
+}
+/************************************************************************
+ * @fn				- SPI_ReceiveData
+ *
+ * @brief			-
+ *
+ * @param[in]		-
+ * @param[in]		-
+ * @param[in]		-
+ *
+ * @return			- none
+ *
+ * @Note			- none
+ ************************************************************************/
+void SPI_IRQHandle(SPI_Handle_t *pHandle)
+{
+
+}
+/*
+ * Other Peripheral and ISR handling
+ */
