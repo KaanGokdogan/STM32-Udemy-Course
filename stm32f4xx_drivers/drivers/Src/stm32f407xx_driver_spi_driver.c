b9021ee9936902eb88119ab5cdc09fa8661e407f
KaanGokdogan <kaan.gkdoan93@gmail.com>
2025-03-02 23:04:53 +0300

    SPI receive data exercise

--- stm32f4xx_drivers/drivers/Src/stm32f407xx_driver_spi_driver.c
+++ stm32f4xx_drivers/drivers/Src/stm32f407xx_driver_spi_driver.c
@@ -228,7 +228,30 @@ void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer,uint32_t Length)
  ************************************************************************/
 void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer,uint32_t Length)
 {
+	while(Length > 0)
+	{
+		// 1. Wait until RXNE is set
+		// potential to block permanently, POLLING
+		while(SPI_GetFlagStatus(pSPIx, SPI_RXNE_FLAG) == FLAG_RESET);
 
+		// 2. check the DFF bit in CR1
+		if(pSPIx->CR1 & (1 << SPI_CR1_DFF))
+		{
+			// 16 bit DFF
+			// 1. load the data from DR to RxBuffer address
+			*( (uint16_t*)pRxBuffer) = pSPIx->DR;
+			Length--;
+			Length--;
+			(uint16_t*)pRxBuffer++;
+		}
+		else
+		{
+			// 8 bit DFF
+			*pRxBuffer = pSPIx->DR;
+			Length--;
+			pRxBuffer++;
+		}
+	}
 }
 
 
