ba541575328be3d5c08c6f830ca4b4ff34d565a7
KaanGokdogan <kaan.gkdoan93@gmail.com>
2025-03-03 00:21:35 +0300

    Revert "SPI receive data exercise"
    This reverts commit b9021ee9936902eb88119ab5cdc09fa8661e407f.

--- stm32f4xx_drivers/drivers/Inc/stm32f407xx_driver_spi_driver.h
+++ stm32f4xx_drivers/drivers/Inc/stm32f407xx_driver_spi_driver.h
@@ -122,7 +122,6 @@ void SPI_IRQHandle(SPI_Handle_t *pHandle);
 void SPI_PeripheralControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi);
 void SPI_SSIConfig(SPI_RegDef_t *pSPIx, uint8_t EnorDi);
 void SPI_SSOEConfig(SPI_RegDef_t *pSPIx, uint8_t EnorDi);
-uint8_t SPI_GetFlagStatus(SPI_RegDef_t *pSPIx, uint32_t FlagName);
 
 
 #endif /* INC_STM32F407XX_DRIVER_SPI_DRIVER_H_ */
